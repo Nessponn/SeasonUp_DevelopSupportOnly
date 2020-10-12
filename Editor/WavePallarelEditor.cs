36139007dc8ddab19c817dd7c937ca5ae4c9db91
Nessponn <w8f9685fd7m758aW23YW@softbank.ne.jp>
2020-10-12 17:56:43 +0900

    セーブ３　エディター拡張で視認性アップ

---
+++ Editor/WavePallarelEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WaterPallarel))]
+
+public class WavePallarelEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        WaterPallarel WP = target as WaterPallarel;
+        WP.WaveSetting = EditorGUILayout.Foldout(WP.WaveSetting, "波の設定");
+        if (WP.WaveSetting)
+        {
+            WP.Width = EditorGUILayout.FloatField("横幅",WP.Width);
+            WP.Bottom = EditorGUILayout.FloatField("深さ",WP.Bottom);
+            WP.WavePower = EditorGUILayout.FloatField("波のパワー",WP.WavePower);
+            WP.WaveSpeed = EditorGUILayout.FloatField("波のスピード（処理能力に注意）",WP.WaveSpeed);
+            WP.SplashPower = EditorGUILayout.FloatField("水しぶきの飛散力",WP.SplashPower);
+        }
+
+        WP.OtherSetting = EditorGUILayout.Foldout(WP.OtherSetting, "その他の設定");
+        if (WP.OtherSetting)
+        {
+            WP.splash = EditorGUILayout.ObjectField("水しぶきのパーティクル",WP.splash,typeof (GameObject),true)as GameObject;
+            WP.mat = EditorGUILayout.ObjectField("テクスチャマテリアル", WP.mat, typeof(GameObject), true) as Material;
+            WP.watermesh = EditorGUILayout.ObjectField("テクスチャマテリアル", WP.watermesh, typeof(GameObject), true) as GameObject;
+        }
+    }
+}
--- wave/WaterPallarel.cs
+++ wave/WaterPallarel.cs
@@ -9,127 +9,89 @@ using Unity.Jobs.LowLevel;
 using Unity.Collections;
 public class WaterPallarel : MonoBehaviour
 {
-    
+
     //可変数
-    public Vector4 WaveElements;
+
+    [SerializeField] public bool WaveSetting;
+    public float Width;
+    public float Bottom;
     public float WavePower = 1;
+    public float WaveSpeed = 5;
     public float SplashPower = 2.5f;
 
     //拡張機能
     [SerializeField] private bool Waver;//常に波を打つか
 
-    //Our renderer that'll make the top of the water visible
-    LineRenderer Body;
+    //物理演算の結果を書き込み、読み込みを行う
+    private NativeArray<Vector3> Vertices;
+    private NativeArray<float> xpositions;
+    private NativeArray<float> ypositions;
+    private NativeArray<float> velocities;
+    private NativeArray<float> accelerations;
 
-    //Our physics arrays
-    Vector3[] Vertices;
-    NativeArray<float> xpositions;
-    NativeArray<float> ypositions;
-    NativeArray<float> velocities;
-    NativeArray<float> accelerations;
-
-    float[] t;
-
-
-    //Our meshes and colliders
+    //メッシュとコライダー
     GameObject[] meshobjects;
     GameObject[] colliders;
     Mesh meshes;
 
-    //Our particle system
+    [SerializeField] public bool OtherSetting;
+
+    //水しぶきのパーティクル
     public GameObject splash;
 
-    //The material we're using for the top of the water
+    //表面上の波の描画をするためのテクスチャマテリアル
     public Material mat;
 
-    //The GameObject we're using for a mesh
+    //実際のメッシュ
     public GameObject watermesh;
 
-    //All our constants
+    //物理演算に必要な定数
     const float springconstant = 0.02f;//バネ定数
     const float damping = 0.04f;//減衰係数。こいつのおかげで弾力のある波が生成される
     const float spread = 0.05f;
     const float z = 10f;
 
-    //The properties of our water
+    //元の波の高さ
     float baseheight;
-    float left;
-    float bottom;
-
-    private float ft = 0;//波を常時揺らしたいときに
 
     private int edgecount = 0;
     private int nodecount = 0;
 
     JobHandle _jobHandle;
-    //JobHandle _JobNodeUpd;
-    //JobHandle _JobWaveAlways;
+
     void Start()
     {
-        //Spawning our water
-        //SpawnWater(-10,20,0,-3);
-        SpawnWater(WaveElements.x, WaveElements.y, WaveElements.z, WaveElements.w);
+        SpawnWater(Width, Bottom);
     }
     //スタート時に生成。設定をするだけなのでここではJobSystemは用いない
-    public void SpawnWater(float Left, float Width, float Top, float Bottom)
+    public void SpawnWater(float Width, float Bottom)
     {
-        //Bonus exercise: Add a box collider to the water that will allow things to float in it.
         //判定を取り付ける
         gameObject.AddComponent<BoxCollider2D>();
-        gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(Left + Width / 2, (Top + Bottom) / 2);
-        gameObject.GetComponent<BoxCollider2D>().size = new Vector2(Width, Top - Bottom);
+        gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(this.transform.position.x + Width / 2, (this.transform.position.y + Bottom) / 2);
+        gameObject.GetComponent<BoxCollider2D>().size = new Vector2(Width, this.transform.position.y - Bottom);
         gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
 
-
-        //Calculating the number of edges and nodes we have
-        //edgecountは
-        edgecount = 20;//設定する頂点の数。重くなりすぎるとやばいので２０で固定。
-        nodecount = edgecount + 1;//波に適用される実際の頂点（節目、節合点）。きちんと設定しなければ
-        Debug.Log("edgecount = " + edgecount);
-        Debug.Log("nodecount =" + nodecount);
-
-        //Add our line renderer and set it up:
-        Body = gameObject.AddComponent<LineRenderer>();
-        Body.material = mat;
-        Body.material.renderQueue = 1000;
-        Body.SetVertexCount(nodecount);
-        Body.SetWidth(0.1f, 0.1f);
-
-        //Declare our physics arrays
-        /*
-        xpositions = new float[nodecount];
-        ypositions = new float[nodecount];
-        velocities = new float[nodecount];
-        accelerations = new float[nodecount];//加速度。LineRendererで設定した頂点がそれぞれもつことになる
-        */
+        edgecount = Mathf.RoundToInt(Width) * 5;//設定する頂点の数。
+        nodecount = edgecount + 1;//波に適用される実際の頂点（節目、節合点）。きちんと設定しなければぐちゃぐちゃになる
 
         xpositions = new NativeArray<float>(nodecount, Allocator.Persistent);
         ypositions = new NativeArray<float>(nodecount, Allocator.Persistent);
         velocities = new NativeArray<float>(nodecount, Allocator.Persistent);
         accelerations = new NativeArray<float>(nodecount, Allocator.Persistent);
 
-        t = new float[nodecount];
-
-        for (int i = 0; i < nodecount; i++)
-        {
-            t[i] = (nodecount % 3 == 0 ? 2 : (nodecount % 2 == 0 ? 1 : 0));
-        }
-
-        //Declare our mesh arrays
+        //メッシュと波の他オブジェクトの感知用コライダー
         meshobjects = new GameObject[edgecount];
         colliders = new GameObject[edgecount];
 
-        //インスペクター内部で設定した
-        baseheight = Top;
-        bottom = Bottom;
-        left = Left;
+        //もとの高さに戻るように基準を保存
+        baseheight = this.transform.position.y;
 
         //For each node, set the line renderer and our physics arrays
         for (int i = 0; i < nodecount; i++)
         {
-            ypositions[i] = Top;
-            xpositions[i] = Left + (Width * i) / edgecount;//Leftに設定された値を基準にwidthで指定した広さまで頂点を展開する
-            Body.SetPosition(i, new Vector3(xpositions[i], Top, z));//上記で生成した頂点を即座にLineRendererに適用する
+            ypositions[i] = this.transform.position.y;
+            xpositions[i] = this.transform.position.x + (Width * i) / edgecount;//Leftに設定された値を基準にwidthで指定した広さまで頂点を展開する
 
             //上記のposition関連の処理はあくまで値を決めただけで、「座標ではない」
             //座標の取り決めはmeshを生成するときに用いる
@@ -138,46 +100,24 @@ public class WaterPallarel : MonoBehaviour
             velocities[i] = 0;
         }
 
-        //Setting the meshes now:
-
-        //Make the mesh
+        //メッシュつくるー
         meshes = new Mesh();
 
-        //Create the corners of the mesh
-
-        Vertices = new Vector3[(nodecount * 2)];
+        //メッシュの要素いろいろ。
+        Vertices = new NativeArray<Vector3>(nodecount * 2, Allocator.Persistent);
         Vector2[] UVs = new Vector2[nodecount * 2];
         for (int i = 0; i < nodecount; i++)
         {
-            float r = Left + (Width * i) / edgecount;
+            //頂点とUVを生成する。頂点はJobSystemで操作しやすいようにするためにこのような生成をしている。UVはその頂点の生成法に沿って一緒に作っている。
+            float r = this.transform.position.x + (Width * i) / edgecount;
             Vertices[i * 2] = new Vector3(xpositions[i], ypositions[i], z);//上辺の頂点、左上
-            Vertices[i * 2 + 1] = new Vector3(xpositions[i], bottom, z);//下辺の頂点、左下
+            Vertices[i * 2 + 1] = new Vector3(xpositions[i], Bottom, z);//下辺の頂点、左下
             UVs[i * 2] = new Vector2(r, 1f);
             UVs[i * 2 + 1] = new Vector2(r, 0f);
-            /*
-            Vertices[i] = new Vector3(xpositions[i], ypositions[i], z);//上辺の頂点、左上
-            Vertices[i + 1] = new Vector3(xpositions[i], bottom, z);//下辺の頂点、左下
-            Vertices[i + 2] = new Vector3(xpositions[i + 1], ypositions[i + 1], z);//上辺の頂点、右上
-            Vertices[i + 3] = new Vector3(xpositions[i + 1], bottom, z);//下辺の頂点、右下
-            */
         }
-
-       
-        for (int i = 0; i < nodecount - 1; i+=2)
-        {
-            //Set the UVs of the texture
-
-            /*
-            UVs[i] = new Vector2(0, i / nodecount);
-            UVs[i + 1] = new Vector2(0, 0);
-            UVs[i + 2] = new Vector2(i / nodecount, i / nodecount);
-            UVs[i + 3] = new Vector2(i / nodecount, 0);
-            */
-        }
-
 
         int[] tris = new int[3 * 2 * (nodecount - 1)];
-        //Set where the triangles should be.
+        //頂点同士で三角形を作ってつなげる作業。
         for (int i = 0; i < nodecount - 1; i++)
         {
             tris[i * 6 + 0] = i * 2;
@@ -190,14 +130,14 @@ public class WaterPallarel : MonoBehaviour
 
 
 
-        //Add all this data to the mesh.
-        meshes.vertices = Vertices;
+        //上記までの生成結果をMeshに反映
+            meshes.vertices = Vertices.ToArray();
             meshes.uv = UVs;
             meshes.triangles = tris;
 
         for (int i = 0; i < nodecount - 1; i++)
         {
-            //Create a holder for the mesh, set it to be the manager's child
+            //メッシュを波の四角の数だけ張り付ける。
             meshobjects[i] = Instantiate(watermesh, Vector3.zero, Quaternion.identity);
             meshobjects[i].GetComponent<MeshFilter>().mesh = meshes;
             meshobjects[i].transform.parent = transform;
@@ -207,27 +147,27 @@ public class WaterPallarel : MonoBehaviour
         
         
         
-        for (int i = 0; i < nodecount - 1 ; i+=2)
+        for (int i = 0; i < nodecount - 1 ; i++)
         {
-            //Create our colliders, set them be our child
+            //子オブジェクトとしてトリガーコライダーをセット
             colliders[i] = new GameObject();
-            colliders[i].name = "Trigger";
+            colliders[i].name = "WaveCollider";
             colliders[i].AddComponent<BoxCollider2D>();
             colliders[i].transform.parent = transform;
 
-            //Set the position and scale to the correct dimensions
-            colliders[i].transform.position = new Vector3(Left + Width * (i + 0.5f) / nodecount * 2, Top - 0.5f, 0);
+            //適切なサイズにコライダーをリサイズ
+            colliders[i].transform.position = new Vector3(this.transform.position.x + Width * (i + 0.5f) / nodecount * 2, this.transform.position.y - 0.5f, 0);
             colliders[i].transform.localScale = new Vector3(Width / nodecount * 2, 1, 1);
 
-            //Add a WaterDetector and make sure they're triggers
+            //各コライダーの設定を変更、反映
             colliders[i].GetComponent<BoxCollider2D>().isTrigger = true;
             colliders[i].AddComponent<WaterDetectorPallarel>();
-
-            Debug.Log("i ="+i/5);
         }
         
     }
-    
+
+
+    private float ft = 0;//波を常時揺らしたいときにid
     void AlwaysWaver()//拡張機能。常に波を揺らす
     {
         NativeArray<float> nodeWaver = new NativeArray<float>(nodecount, Allocator.TempJob);
@@ -254,7 +194,7 @@ public class WaterPallarel : MonoBehaviour
 
         _jobHandle.Complete();
 
-        Debug.Log("ypositions = " + ypositions.Length);
+        //Debug.Log("ypositions = " + ypositions.Length);
 
         for (int i = 0; i < nodecount; i++)
         {
@@ -291,10 +231,10 @@ public class WaterPallarel : MonoBehaviour
         //jobの実行（繰り返す回数,処理分割回数）
         
         //LineRendererの座標更新（LineRendererはIJobPallarelでは使えない）
-        for (int i = 0; i < xpositions.Length; i++)
-        {
-            Body.SetPosition(i, new Vector3(xpositions[i], ypositions[i], z));
-        }
+        //for (int i = 0; i < xpositions.Length; i++)
+        //{
+            //Body.SetPosition(i, new Vector3(xpositions[i], ypositions[i], z));
+       // }
 
 
         // nodeUpdate();//次はnodeを更新する
@@ -396,33 +336,13 @@ public class WaterPallarel : MonoBehaviour
         public NativeArray<float> wrb1;
         public void Execute(int index)//8を入れる(なんかindexの値と配列で参照する番号が一致していないとエラーが起きるっぽい？)
         {
-            /*
-            if (index > 0)
-            {
-                float wrb1 = spread * (ypositions[index] - ypositions[index - 1]);
-                velocityDeltas[index - 1] += wrb1;
-                ypositionDeltas[index - 1] += wrb1;
-            }
-            if (index < xpositions.Length - 1)
-            {
-                float wrb2 = spread * (ypositions[index] - ypositions[index + 1]);
-                velocityDeltas[index + 1] += wrb2;
-                ypositionDeltas[index + 1] += wrb2;
-            }
-            */
-
-            //Debug.Log("index = " + index);
             if (index > 0)
             {
                 wlb1[index] = spread * (ypositions[index] - ypositions[index-1]);
-                //velocityDeltas[index] += wrb1;
-                //ypositionDeltas[index] += wrb1;
             }
             if (index < xpositions.Length-1)
             {
                 wrb1[index] = spread * (ypositions[index] - ypositions[index+1]);
-                //velocityDeltas[index] += wrb2;
-                //ypositionDeltas[index] += wrb2;
             }
 
         }
@@ -432,28 +352,25 @@ public class WaterPallarel : MonoBehaviour
     struct UpdateMeshesParallel : IJobParallelFor
     {
         public NativeArray<Vector3> Vertices;
-        public float bottom;
         [ReadOnly] public NativeArray<float> xpositions;
         [ReadOnly] public NativeArray<float> ypositions;
 
-        public void Execute(int i)
+        public void Execute(int i)//ここでメッシュの更新を行う
         {
-            if (i % 2 == 0) Vertices[i] = new Vector3(xpositions[i], ypositions[i], z);
-            else Vertices[i] = new Vector3(xpositions[i - 1], bottom, z);
-            //Vertices[i] = new Vector3(xpositions[i % 4 == 0? i : i + 1], ypositions[i % 4 == 0 ? i : i + 1], z);
+            if (i % 2 == 0) Vertices[i] = new Vector3(xpositions[i/2], ypositions[i/2], z);
         }
     }
 
     void UpdateMeshes()
     {
-        /*
-        NativeArray<Vector3> Vertices = new NativeArray<Vector3>(nodecount * 2, Allocator.TempJob);
+
+        meshes.vertices = Vertices.ToArray();//更新の反映
+
         UpdateMeshesParallel UMP = new UpdateMeshesParallel
         {
             Vertices = Vertices,
-            bottom = bottom,
             xpositions = xpositions,
-            ypositions = xpositions,
+            ypositions = ypositions,
         };
         _jobHandle = UMP.Schedule(Vertices.Length, 0);
 
@@ -461,22 +378,20 @@ public class WaterPallarel : MonoBehaviour
 
         _jobHandle.Complete();
 
-        meshes.vertices = Vertices.ToArray();
-
-        Vertices.Dispose();
-        */
+        
 
         
         
+
+        
+        /*
         for (int i = 0; i < nodecount; i++)
         {
-            
             Vertices[i*2] = new Vector3(xpositions[i], ypositions[i], z);
-            
         }
         
         meshes.vertices = Vertices;
-        
+        */
     }
 
     public void Splash(float xpos, float velocity_x, float velocity)
@@ -524,12 +439,18 @@ public class WaterPallarel : MonoBehaviour
             float lifetime = 0.93f + Mathf.Abs(velocity) * 0.07f;
 
             //Set the splash to be between two values in Shuriken by setting it twice.
-            if (velocity <= -10) velocity = velocity;
-            if (velocity <= -20) velocity = -20;
+            //速度によって生成する水しぶきの密度を変更
+            int splashCount = (int)Mathf.Abs(velocity_x) + (int)Mathf.Abs(velocity);
+
+            if (velocity <= -7) velocity = -7;
+            //if (velocity <= -20) velocity = -20;
             
-            splash.GetComponent<ParticleSystem>().startSpeed = ((Mathf.Abs(velocity) * SplashPower ) * 2 )  * Mathf.Pow(Mathf.Abs(velocity) + 1, Mathf.Abs(velocity)  + 1);
+            splash.GetComponent<ParticleSystem>().startSpeed = ((Mathf.Abs(velocity) * SplashPower ) * 2 )  * Mathf.Pow(Mathf.Abs(velocity) + 1, Mathf.Abs(velocity)  + 1) + 1;
             //splash.GetComponent<ParticleSystem>().startSpeed = (Mathf.Abs(velocity) * 2) + 2 * Mathf.Pow(Mathf.Abs(velocity) + 1, Mathf.Abs(velocity) + 1);
             splash.GetComponent<ParticleSystem>().startLifetime = lifetime;
+
+            if (splash.GetComponent<ParticleSystem>().startSpeed > 10) splash.GetComponent<ParticleSystem>().startSpeed = 10;
+
             //Debug.Log("velocity = "+velocity);
             //Set the correct position of the particle system.
             Vector3 position = new Vector3(xpositions[index], ypositions[index] - 0.35f, 5);
@@ -538,8 +459,9 @@ public class WaterPallarel : MonoBehaviour
             Quaternion rotation = Quaternion.LookRotation(new Vector3(xpositions[Mathf.FloorToInt(xpositions.Length / 2)], baseheight + 8, 5) - position);
 
             //Create the splash and tell it to destroy itself.
-            GameObject splish = Instantiate(splash, position, rotation) as GameObject;
-            Destroy(splish, lifetime + 0.3f);
+            //GameObject splish = Instantiate(splash, position, rotation) as GameObject;
+            //Destroy(splish, lifetime + 0.3f);
+            for(int i = 0; i < splashCount; i++) Destroy(Instantiate(splash, position, rotation), lifetime + 0.3f);
         }
     }
 
@@ -549,7 +471,7 @@ public class WaterPallarel : MonoBehaviour
         ypositions.Dispose();
         velocities.Dispose();
         accelerations.Dispose();
-        
+        Vertices.Dispose();
     }
 
 
