0fbeef722129ff558d99b55ba2e0a74eb01ac475
Nessponn <w8f9685fd7m758aW23YW@softbank.ne.jp>
2020-12-03 02:27:33 +0900

    敵のファイル改善前

---
+++ EnemyObj.cs
@@ -0,0 +1,339 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class EnemyObj : MonoBehaviour,DamagableObject,AI_Logic_Enemy
+{
+    public EnemyData Enemydata;
+    //あとでEnemyObjectっていうクラス名に変えようかな…
+
+    //このスクリプトは、ジャンプなどの行動を行うとともに
+
+    enum _MoveDir
+    {
+        Left,Right,Wait
+    }
+    private _MoveDir movedirection = _MoveDir.Wait;
+
+    private Rigidbody2D rbody;
+
+    private bool FloorTaken = false;//接地しているかどうか。
+    private bool iceFloorTaken = false;//氷の床に接地しているかどうか。
+
+    private bool _DEATH;//やられたかどうか
+
+    //ステータス
+    private int HP;
+
+    //private GameObject Player;
+    private void Start()
+    {
+        //ゲームオブジェクトを定義し、敵の情報を格納した後そのオブジェクトを子オブジェクトに登録する
+        //サークルコライダーをつける
+        //Enemydata = transform.parent.GetComponent<EnemySpawner>().GetEnemyData();
+
+        //データの定義
+        this.transform.localScale = new Vector3(Enemydata.EnemySpriteSetting.Scale, Enemydata.EnemySpriteSetting.Scale, 1);
+        HP = Enemydata.EnemyObjectSetting.HP;
+        rbody = GetComponent<Rigidbody2D>();
+        
+    }
+    private void Update()
+    {
+        /*
+        if (DebugMode)
+        {
+            enemy.GetComponent<Rigidbody2D>().mass = EnemyData.RigidBodySetting.Mass;
+            enemy.GetComponent<Rigidbody2D>().gravityScale = EnemyData.RigidBodySetting.Gravity;
+            if (EnemyData.ColliderSetting.BoxCol)
+            {
+                //敵の判定とは別に、着地を行う足の判定をつけるための判定
+                enemy.GetComponentInChildren<BoxCollider2D>().size = new Vector2(EnemyData.ColliderSetting.Boxsize_x, EnemyData.ColliderSetting.Boxsize_y);
+                enemy.GetComponentInChildren<BoxCollider2D>().offset = new Vector2(enemy.GetComponentInChildren<BoxCollider2D>().offset.x, EnemyData.ColliderSetting.Boxoffset_y);
+            }
+        }
+
+        */
+        if(this.transform.position.y <= -10)
+        {
+            Destroy(this.gameObject);
+        }
+
+        var Player = GameObject.FindWithTag("Player");
+
+        if (!_DEATH)//やられているのに行動されちゃあまずいからさ…
+        {
+            //一定距離までプレイヤーが敵に近づいたら行動開始
+            if (Mathf.Abs(this.gameObject.transform.localPosition.x - Player.transform.localPosition.x) < Enemydata.AgainstPlayerSetting.PlayerSearchRange)
+            {
+                _ConstraintsCheck();
+                _move();
+                    //移動中、プレイヤーが一定の高さ以上に位置していたらジャンプ行動を開始する
+                    
+                if (Player.transform.localPosition.y - this.transform.localPosition.y > 2f)
+                {
+                   
+                    _jump();
+                }
+            }
+            else
+            {
+                movedirection = _MoveDir.Wait;
+            }
+        }
+
+        switch (movedirection)
+        {
+            case _MoveDir.Left:
+
+                //anim.SetFloat("Run", Mathf.Abs(-3));
+
+                //空中と地上(雪地)と地上（氷面）で加速度が違う
+                if (iceFloorTaken && FloorTaken) rbody.AddForce(new Vector2(-Enemydata.EnemyObjectSetting.Speed_OnIce, 0));
+                else if (FloorTaken) rbody.AddForce(new Vector2(-Enemydata.EnemyObjectSetting.Speed, 0));
+                else rbody.AddForce(new Vector2(-Enemydata.EnemyObjectSetting.Speed_InAir, 0));
+
+                //または、サーチ範囲内にいるにもかかわらず、何らかの原因で速度が激減した場合もジャンプ行動を行う
+                if (rbody.velocity.x >= 0) _jump();
+
+                if (rbody.velocity.x < -Enemydata.EnemyObjectSetting.MaxSpeed) rbody.velocity = new Vector2(-Enemydata.EnemyObjectSetting.MaxSpeed, rbody.velocity.y);
+                GetComponent<SpriteRenderer>().flipX = false;
+                break;
+            case _MoveDir.Right:
+                //anim.SetFloat("Run", Mathf.Abs(3));
+
+                //空中と地上(雪地)と地上（氷面）で加速度が違う
+                if (iceFloorTaken && FloorTaken) rbody.AddForce(new Vector2(Enemydata.EnemyObjectSetting.Speed_OnIce, 0));
+                else if (FloorTaken) rbody.AddForce(new Vector2(Enemydata.EnemyObjectSetting.Speed, 0));
+                else rbody.AddForce(new Vector2(Enemydata.EnemyObjectSetting.Speed_InAir, 0));
+
+                if (rbody.velocity.x <= 0) _jump();
+
+                if (rbody.velocity.x > Enemydata.EnemyObjectSetting.MaxSpeed) rbody.velocity = new Vector2(Enemydata.EnemyObjectSetting.MaxSpeed, rbody.velocity.y);
+                GetComponent<SpriteRenderer>().flipX = true;
+                break;
+            case _MoveDir.Wait:
+
+                //anim.SetFloat("Run", Mathf.Abs(0));
+                break;
+        }
+
+        if (_Damage)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color =
+                new Color(1f, 1f, 1f, Mathf.Abs(Mathf.Sin(Time.time * 10)));//無敵が発動すると点滅を開始);
+
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().color =
+                new Color(1f, 1f, 1f, 1f);//無敵が発動すると点滅を開始
+        }
+    }
+
+    public void _move()
+    {
+        var Player = GameObject.FindWithTag("Player");
+        //Debug.Log("this.transform.position.x  - Player.transform.localPosition.x = " + (this.transform.position.x - Player.transform.localPosition.x));
+       // Debug.Log("Player.transform.localPosition.x = " + Player.transform.localPosition.x);
+        if (this.transform.position.x - Player.transform.localPosition.x > 0)//右方向にプレイヤーがいた場合
+        {
+            Debug.Log("左にいます！");
+            movedirection = _MoveDir.Left;
+        }
+        else
+        {
+            Debug.Log("右にいます！");
+            movedirection = _MoveDir.Right;
+        }
+    }
+
+    public void _jump()
+    {
+        //そもそも接地判定があるのか
+        //無ければそもそもジャンプする必要はない
+        if (Enemydata.ColliderSetting.BoxCol)
+        {
+
+            if (FloorTaken || iceFloorTaken) rbody.velocity = new Vector2(rbody.velocity.x, Enemydata.EnemyObjectSetting.JumpPower);
+
+        }
+    }
+    //ダメージ処理
+    
+    public void _TakeDamage() 
+    {
+        Debug.Log("敵がやられました！");
+        rbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        GetComponent<CircleCollider2D>().enabled = false;
+        gameObject.GetComponentInChildren<BoxCollider2D>().enabled = false;
+        gameObject.GetComponentInChildren<SpriteRenderer>().flipY = true;
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, 9);
+
+        
+
+        _DEATH = true;
+    }//プレイヤーまたは敵にあたったときのダメージ
+
+    public void _YukidamaDamage()
+    {
+
+    }//雪玉にあたったときの処理
+
+    public void _SpinDamage() 
+    {
+    
+    }//プレイヤーのスピンにあたったとき
+
+    public void _WaterDEATH()
+    {
+    
+    }//水没した時
+
+
+    private void _ConstraintsCheck()
+    {
+        if (Enemydata.RigidBodySetting.constant_z && !Enemydata.RigidBodySetting.PlayerRangeWithconstant_z)
+        {
+            if (Enemydata.RigidBodySetting.constant_x && Enemydata.RigidBodySetting.constant_y&& !Enemydata.RigidBodySetting.PlayerRangeWithconstant_x && !Enemydata.RigidBodySetting.PlayerRangeWithconstant_y)
+            {
+                //ｘ、ｙ、ｚの全てが押されている
+                GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+            else if (Enemydata.RigidBodySetting.constant_y && !Enemydata.RigidBodySetting.PlayerRangeWithconstant_y)
+            {
+                GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+            }//この時点で、zは押されているが、ｙは押されていないということがわかる
+            else if (Enemydata.RigidBodySetting.constant_x && !Enemydata.RigidBodySetting.PlayerRangeWithconstant_x)
+            {
+                GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            }
+            //この時点で、zのみが押されているということが分かった
+            else
+            {
+                GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+            }
+        }
+        //この時点でzが押されているということはなくなった
+        else if (Enemydata.RigidBodySetting.constant_y && !Enemydata.RigidBodySetting.PlayerRangeWithconstant_y)
+        {
+            if (Enemydata.RigidBodySetting.constant_x && !Enemydata.RigidBodySetting.PlayerRangeWithconstant_x)
+            {
+                GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+            }
+            //この時点で、ｙのみが押されているということが分かった
+            else
+            {
+                GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+            }
+        }
+        //この時点でｙが押されているということはなくなった
+        else if (Enemydata.RigidBodySetting.constant_x && !Enemydata.RigidBodySetting.PlayerRangeWithconstant_x)
+        {
+            //ｘのみが押されている
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+        }
+        //この時点で、zのみが押されているということが分かった
+        else
+        {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        }
+    }
+
+
+    private bool _Damage;
+    private IEnumerator Damage()
+    {
+        _Damage = true;
+        yield return new WaitForSeconds(0.5f);
+        _Damage = false;
+    }
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            var obj = col.gameObject.GetComponent<PlayerStatusController>();
+            //大抵falseが帰ってくるので、それで判別
+            //また、スピンペネトレーション（スピン貫通）が働いていたらそれもダメージ通す
+            if (obj.SpinStateGetter() && !Enemydata.AgainstPlayerSetting.PlayerSpinPenetration && !_Damage)
+            {
+                //プレイヤーがスピンしながら突っ込んできた場合、敵は死ぬ
+                HP--;
+                //Debug.Log("HP =" + HP);
+                if(HP == 0)
+                {
+                    //HPが０になったらご退場
+                    _TakeDamage();
+                }
+                else
+                {
+                    //HPがまだ１以上であれば、敵に無敵時間を付与し、行動を続行させる
+                    StartCoroutine(Damage());
+                }
+            }
+            else
+            {
+                //プレイヤーがそのまま突っ込んできたら、プレイヤーにダメージを与える
+                obj._TakeDamage();
+            }
+        }
+        if(col.gameObject.tag == "SHEILD")//ブリザードシールドの判定に触れた場合
+        {
+            HP--;
+            if (HP == 0)
+            {
+                //HPが０になったらご退場
+                _TakeDamage();
+            }
+            else
+            {
+                //HPがまだ１以上であれば、敵に無敵時間を付与し、行動を続行させる
+                StartCoroutine(Damage());
+            }
+        }
+
+        string LayerName = LayerMask.LayerToName(col.gameObject.layer);
+        if ((LayerName == "floor" ))
+        {
+            FloorTaken = true;
+        }
+        if (col.gameObject.tag == "icefloor")
+        {
+            iceFloorTaken = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        //string layername = LayerMask.LayerToName(col.gameObject.layer);
+        //if (layername == "floor") _Jump = true;
+        string LayerName = LayerMask.LayerToName(col.gameObject.layer);
+        if ((LayerName == "floor"))
+        {
+            FloorTaken = true;
+        }
+        if (col.gameObject.tag == "icefloor")
+        {
+            iceFloorTaken = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        //string layername = LayerMask.LayerToName(col.gameObject.layer);
+        //if (layername == "floor") _Jump = false;
+        string LayerName = LayerMask.LayerToName(col.gameObject.layer);
+        if ((LayerName == "floor"))
+        {
+            FloorTaken = false;
+        }
+        if (col.gameObject.tag == "icefloor")
+        {
+            iceFloorTaken = false;
+        }
+    }
+}
--- EnemySpawner.cs
+++ EnemySpawner.cs
@@ -1,121 +1,182 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class EnemySpawner : MonoBehaviour,DamagableObject,AI_Logic_Enemy
+//[ExecuteInEditMode]…実行したものが、エディターの再生を止めても残り続ける設定
+public class EnemySpawner : MonoBehaviour
 {
-    //あとでEnemyObjectっていうクラス名に変えようかな…
+    //EnemyObjを発生させるためのもの。
 
-    enum _MoveDir
+    //スポナーにつけるもの
+    //こいつ（EnemySpawner）、SpriteRenderer、エディター上にスプライトを表示させるための更新ボタン「スプライト更新」
+
+    public EnemyData EnemyData;
+
+    public GameObject EnemyBase;
+
+    private bool Respawn = true;
+
+    private GameObject enemy;
+
+    public bool DebugMode;
+
+    // Update is called once per frame
+    void Update()
     {
-        Left,Right,Wait
-    }
-    private _MoveDir movedirection = _MoveDir.Wait;
-
-    private Rigidbody2D Rbody;
-    [SerializeField] private EnemyData Enemydata;
-
-    private bool _Jump;
-
-
-    private void Start()
-    {
-        //
-        Rbody = GetComponent<Rigidbody2D>();
-    }
-    private void Update()
-    {
-        //プレイヤーのサーチ
-        var Player = GameObject.FindWithTag("Player");
-        //一定距離までプレイヤーが敵に近づいたら行動開始
-        if (Mathf.Abs(this.transform.localPosition.x - Player.transform.localPosition.x) < Enemydata.AgainstPlayerSetting.PlayerSearchRange)
-        {
-            _move();
-
-            //移動中、プレイヤーが一定の高さ以上に位置していたらジャンプ行動を開始する
-            if(Player.transform.localPosition.y - this.transform.localPosition.y > 2)
-            {
-                _jump();
-            }
-
-        }
-        else
-        {
-            movedirection = _MoveDir.Wait;
-        }
-
-        switch (movedirection)
-        {
-            case _MoveDir.Left:
-
-                if (_Jump) Rbody.AddForce(new Vector2(-Enemydata.EnemyObjectSetting.Speed, 0));
-                else Rbody.AddForce(new Vector2(-Enemydata.EnemyObjectSetting.Speed_InAir, 0));
-
-                if (Rbody.velocity.x < -Enemydata.EnemyObjectSetting.MaxSpeed) Rbody.velocity = new Vector2(-Enemydata.EnemyObjectSetting.MaxSpeed, Rbody.velocity.y);
-                GetComponent<SpriteRenderer>().flipX = true;
-
-
-
-                break;
-            case _MoveDir.Right:
-                if (_Jump) Rbody.AddForce(new Vector2(Enemydata.EnemyObjectSetting.Speed, 0));
-                else Rbody.AddForce(new Vector2(Enemydata.EnemyObjectSetting.Speed_InAir, 0));
-
-                if (Rbody.velocity.x > Enemydata.EnemyObjectSetting.MaxSpeed) Rbody.velocity = new Vector2(Enemydata.EnemyObjectSetting.MaxSpeed, Rbody.velocity.y);
-                break;
-            case _MoveDir.Wait:
-                break;
-        }
-    }
-
-    public void _move()
-    {
-        var Player = GameObject.FindWithTag("Player");
-        if(this.transform.localPosition.x - Player.transform.localPosition.x > 0)//右方向にプレイヤーがいた場合
-        {
-            movedirection = _MoveDir.Left;
-        }
-        else
-        {
-            movedirection = _MoveDir.Right;
-        }
-    }
-
-    public void _jump()
-    {
-        if (_Jump) Rbody.velocity = new Vector2(Rbody.velocity.x,Enemydata.EnemyObjectSetting.JumpPower);
         
+        var Player = GameObject.FindWithTag("Player");
+        //Debug.Log("this.gameObject.transform.localPosition.x - Player.transform.localPosition.x = " + (this.gameObject.transform.localPosition.x - Player.transform.localPosition.x));
+        if (Mathf.Abs(this.gameObject.transform.localPosition.x - Camera.main.transform.localPosition.x) < 10)
+        {//範囲内にプレイヤーがいるかどうか
+            
+            //敵が生成されていない、かつリスポーン設定がされていた場合
+            if (!enemy && Respawn)
+            {
+                Respawn = false;
+                EnemySpawn();
+            }
+            //生成後にRespawnの状態を上書きする（つまり一度はかならず生成されるようにする）
+        }
+        else
+        {
+            if (!enemy) Respawn = EnemyData.EnemyObjectSetting.Reapawn;
+        }
+
     }
-    //ダメージ処理
-    
-    public void _TakeDamage() 
-    {
-        Debug.Log("敵がやられました！");
-    }//プレイヤーまたは敵にあたったときのダメージ
 
-    public void _YukidamaDamage()
+    void EnemySpawn()//敵を生成する
     {
+        //あーガバガバ…
+        //子オブジェクトに子オブジェクトはつけらんないので、仕方がないので敵の型のプレハブ作ります
+        //プレハブの敵ベースとなるオブジェクトに様々なデータ情報を送り込むという形にします
 
-    }//雪玉にあたったときの処理
+        enemy = Instantiate(EnemyBase, this.transform.position, Quaternion.identity);
 
-    public void _SpinDamage() 
-    {
-    
-    }//プレイヤーのスピンにあたったとき
+        enemy.transform.localPosition = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+        enemy.GetComponent<CircleCollider2D>().isTrigger = true;
+        //enemy.gameObject.layer = LayerMask.NameToLayer("Enemy");
 
-    public void _WaterDEATH()
-    {
-    
-    }//水没した時
+        //敵を形成する判定として必要なものを渡す
+        //enemy.GetComponent<CircleCollider2D>().size = new Vector2(1, enemy.transform.localScale.y);
+        enemy.GetComponent<CircleCollider2D>().offset = new Vector2(0, EnemyData.ColliderSetting.Circleoffset_y);
+        enemy.GetComponent<CircleCollider2D>().radius = EnemyData.ColliderSetting.radius;
+        
 
-    private void OnCollisionStay2D(Collision2D col)
-    {
-        string layername = LayerMask.LayerToName(col.gameObject.layer);
-        if (layername == "floor") _Jump = true;
+
+        if (EnemyData.ColliderSetting.BoxCol)
+        {//着地を有効にする
+            
+            //敵の判定とは別に、着地を行う足の判定をつけるための判定
+            enemy.GetComponentInChildren<BoxCollider2D>().size = new Vector2(EnemyData.ColliderSetting.Boxsize_x, EnemyData.ColliderSetting.Boxsize_y);
+            enemy.GetComponentInChildren<BoxCollider2D>().offset = new Vector2(EnemyData.ColliderSetting.Boxoffset_x, EnemyData.ColliderSetting.Boxoffset_y);
+            enemy.transform.GetChild(0).gameObject.layer = LayerMask.NameToLayer("Enemy");
+        }
+        else
+        {
+            //着地を無効にし、床をすり抜けるようにする
+            enemy.GetComponentInChildren<BoxCollider2D>().size = new Vector2(EnemyData.ColliderSetting.Boxsize_x, EnemyData.ColliderSetting.Boxsize_y);
+            enemy.GetComponentInChildren<BoxCollider2D>().offset = new Vector2(EnemyData.ColliderSetting.Boxoffset_x, EnemyData.ColliderSetting.Boxoffset_y);
+            enemy.transform.GetChild(0).gameObject.layer = LayerMask.NameToLayer("EnemythroughFloor");
+        }
+
+        //リジッドボディーの設定
+        enemy.GetComponent<Rigidbody2D>().mass = EnemyData.RigidBodySetting.Mass;
+        enemy.GetComponent<Rigidbody2D>().gravityScale = EnemyData.RigidBodySetting.Gravity;
+
+        if (EnemyData.RigidBodySetting.constant_z)
+        {
+            if (EnemyData.RigidBodySetting.constant_x && EnemyData.RigidBodySetting.constant_y)
+            {
+                //ｘ、ｙ、ｚの全てが押されている
+                enemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+            else if (EnemyData.RigidBodySetting.constant_y)
+            {
+                enemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+            }//この時点で、zは押されているが、ｙは押されていないということがわかる
+            else if (EnemyData.RigidBodySetting.constant_x)
+            {
+                enemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            }
+            //この時点で、zのみが押されているということが分かった
+            else
+            {
+                enemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+            }
+        }
+        //この時点でzが押されているということはなくなった
+        else if (EnemyData.RigidBodySetting.constant_y)
+        {
+            if (EnemyData.RigidBodySetting.constant_x)
+            {
+                enemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+            }
+            //この時点で、ｙのみが押されているということが分かった
+            else
+            {
+                enemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+            }
+        }
+        //この時点でｙが押されているということはなくなった
+        else if (EnemyData.RigidBodySetting.constant_x)
+        {
+            //ｘのみが押されている
+            enemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+        }
+        //この時点で、zのみが押されているということが分かった
+        else
+        {
+            enemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        }
+
+        //SpriteRendererのアタッチ
+        enemy.GetComponent<SpriteRenderer>().sprite = EnemyData.EnemySpriteSetting.Image;
+        enemy.gameObject.tag = "Enemy";
+        //敵の行動パターンやデータを入れる.
+        //データの送信は、メソッド開始時にStartメソッドで行われる
+        enemy.AddComponent<EnemyObj>();
+        enemy.GetComponent<EnemyObj>().Enemydata = EnemyData;
+        //エディターで表示しているスポナーのスプライトは消す
+        this.GetComponent<SpriteRenderer>().enabled = false;
     }
-    private void OnCollisionExit2D(Collision2D col)
+
+
+    public void SetSprite()//エディター上でもスプライトの状態を確認できるもの
     {
-        string layername = LayerMask.LayerToName(col.gameObject.layer);
-        if (layername == "floor") _Jump = false;
+        if(EnemyData != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = EnemyData.EnemySpriteSetting.Image;
+
+            //スケールはスポナーのスケールと同じものにしても大丈夫
+            //敵オブジェクトの方には、いいスケールデータの渡し方が今のところ思いつかないのでこのままで…
+            //this.transform.localScale = new Vector3(EnemyData.EnemySpriteSetting.Scale, EnemyData.EnemySpriteSetting.Scale, 1);
+        }
+        else
+        {
+            Debug.LogError("そのスポナーには「EnemyData」がついてません！");
+        }
+    }
+
+    public EnemyData GetEnemyData()
+    {
+        return EnemyData;
+    }
+}
+[CustomEditor(typeof(EnemySpawner))]
+public class SpriteSetter : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();//もともとのインスペクターの内容を表示
+
+        //追加でボタンの表示を行う
+        EnemySpawner spr = target as EnemySpawner;
+
+        if (GUILayout.Button("エディターで表示するスプライトの更新"))
+        {
+            spr.SetSprite();
+        }
+        EditorUtility.SetDirty(target);
     }
 }
